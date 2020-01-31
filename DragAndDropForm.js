f734503d408aa25921ac42965088e68233726eb1
JaakkoKyl <40994736+JaakkoKyl@users.noreply.github.com>
2020-01-31 12:26:04 +0200

    Add files via upload

---
+++ DragAndDropForm.js
@@ -0,0 +1,19 @@
+// JavaScript Document
+
+function ondrop(event)
+	{
+		if (currentDroppable = "drag1")
+	
+	function DragAndDropForm() {
+  var x, text;
+
+  x = document.getElementById("numb").value;
+
+  if (isNaN(x) || x < 1 || x > 10) {
+    text = "Input not valid";
+  } else {
+    text = "Input OK";
+  }
+  document.getElementById("demo").innerHTML = text;
+}
+	}
